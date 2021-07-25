18797f4bd4b9ad2f19320da8a5c5657823aab3f4
dasunNimantha <dasunnimantha77@outlook.com>
2021-07-25 15:47:12 +0530

    aded nginx headers

--- nginx.sh
+++ nginx.sh
@@ -11,6 +11,16 @@ echo "####### SECURING YOUR SERVER  #######"
 echo "#####################################"
 printf "\n"
 
+## nginx conf file configurations
+sudo sed -i "s/# server_tokens off;/server_tokens off;/" /etc/nginx/nginx.conf
+sudo sed -i "/server_tokens off;/a\\
+\\tproxy_hide_header X-Powered-By;\\
+\\tadd_header X-Frame-Options SAMEORIGIN;\\
+\\tadd_header X-Content-Type-Options nosniff;\\
+\\tadd_header X-XSS-Protection \"1; mode=block\";\\
+\\tadd_header Strict-Transport-Security 'max-age=31536000' always;" /etc/nginx/nginx.conf
+
+
 while true; do
     printf "\n"
     read -r -p "Do you want to add SSL certificate via letsencrypt ? (Y/N) : " isSSL
@@ -24,3 +34,5 @@ while true; do
     *) echo "Please answer y or n." ;;
     esac
 done
+
+sudo systemctl restart nginx
